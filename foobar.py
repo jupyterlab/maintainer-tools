03b9d81a4162f87b35cba49a8a14129dd6d74d34
Steven Silvester <steven.silvester@ieee.org>
2022-03-30 13:48:18 -0500

    simplify

--- foobar.py
+++ foobar.py
@@ -1,2 +1,6 @@
 """Stub file for python project"""
 __version__ = "0.0.1"
+
+# Single test to get pytest to pass.
+def test_no_op():
+    pass
--- test.py
+++
@@ -1,4 +0,0 @@
-
-# Single test to get pytest to pass.
-def test_no_op():
-    pass
