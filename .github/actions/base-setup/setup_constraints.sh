9f6b04a917fbb7d5f30aea96ee23bebd0c3967b1
Steven Silvester <steven.silvester@ieee.org>
2023-03-11 13:16:29 -0600

    Print the constraints file to terminal

--- .github/actions/base-setup/setup_constraints.sh
+++ .github/actions/base-setup/setup_constraints.sh
@@ -7,4 +7,5 @@ python -m build --outdir $HOME/dist --wheel .
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 python $SCRIPT_DIR/../install-minimums/create_constraints_file.py $HOME/constraints.txt $HOME/dist/*.whl
+cat $HOME/constraints.txt
 echo "PIP_CONSTRAINT=$HOME/constraints.txt" >> $GITHUB_ENV
