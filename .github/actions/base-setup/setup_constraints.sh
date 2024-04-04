d8ce9ad426af1fd091f048b9f79ce86a6de3e520
Steven Silvester <steve.silvester@mongodb.com>
2024-04-04 09:48:37 -0500

    add missing file

---
+++ .github/actions/base-setup/setup_constraints.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python -m venv $HOME/.venv
+source $HOME/.venv/bin/activate
+pip install build packaging pkginfo
+mkdir $HOME/dist
+python -m build --outdir $HOME/dist --wheel .
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+python $SCRIPT_DIR/../install-minimums/create_constraints_file.py $HOME/constraints.txt $HOME/dist/*.whl
+cat $HOME/constraints.txt
+echo "PIP_CONSTRAINT=$HOME/constraints.txt" >> $GITHUB_ENV
