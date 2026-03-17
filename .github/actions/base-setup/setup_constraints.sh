856a9c0deb4d781cd9269bda751e26a03c95ce64
krassowski <5832902+krassowski@users.noreply.github.com>
2026-03-17 17:02:34 +0000

    Use `uv pip` in more places

--- .github/actions/base-setup/setup_constraints.sh
+++ .github/actions/base-setup/setup_constraints.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 python -m venv $HOME/.venv
 source $HOME/.venv/bin/activate
-pip install build packaging pkginfo
+
+if [ "$PYTHON_PACKAGE_MANAGER" == "uv pip" ]; then
+	uv pip install build packaging pkginfo
+else
+	pip install build packaging pkginfo
+fi
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 python $SCRIPT_DIR/create_constraints_file.py $HOME/constraints.txt $(pwd)
 cat $HOME/constraints.txt
 echo "PIP_CONSTRAINT=$HOME/constraints.txt" >> $GITHUB_ENV
+echo "UV_CONSTRAINT=$HOME/constraints.txt" >> $GITHUB_ENV
+echo "UV_BUILD_CONSTRAINT=$HOME/constraints.txt" >> $GITHUB_ENV
